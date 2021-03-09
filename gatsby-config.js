c1d50008e3ffb07f143ac9fad20015b6c346656a
João Duarte <jaduarte1998@gmail.com>
2021-03-09 20:38:47 +0000

    Remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,28 +14,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          'G-YQCV22G00R', // Google Analytics / GA
-          'G-KGCBX9S4EN',
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          respectDNT: true,
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'JoãoDuarte',
